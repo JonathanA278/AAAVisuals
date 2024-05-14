158e3f4b59fcca8c77c012facae879682a6ef162
JonathanA278 <113187699+JonathanA278@users.noreply.github.com>
2024-05-14 10:04:56 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// document.getElementById('contact-form').addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     // You can add your code to handle form submission (e.g., sending an email) here
+//     alert('Form submitted successfully!');
+//   });
+  
